06fc10d035e9436b3cb6df64ff424cf882039a65
Keith <khengquanlim@gmail.com>
2024-10-15 14:46:21 +0800

    Attempt to integrate search bar, will look to refactor search bar and make it reusable

--- src/components/News/news.jsx
+++ src/components/News/news.jsx
@@ -1,24 +1,45 @@
 import { fetchNewsData } from "../../services/NewsService";
 import { useState, useEffect } from "react";
 import InformationCard from "../InformationCard/InformationCard";
+import StockSearchBar from "../Stock/StockSearchBar/StockSearchBar";
 import { Col, Row, Container } from "react-bootstrap";
 
 import './news.css';
 const API_KEY1 = process.env.API_KEY;
+const newsData = [
+    {
+        authors: ['Ananya Gairola'],
+        banner_image: "https://cdn.benzinga.com/files/images/story/2024/10/13/Apple-Vision-Pro.jpeg?width=1200&height=800&fit=crop",
+        category_within_source : "News",
+        source : "Benzinga",
+        source_domain : "www.benzinga.com",
+        title : "Apple's Low-Cost Vision Pro Could Hit The Shelves Next Year - Everything You Need To Know About iPhone Maker's Plan To Challenge Meta - Meta Platforms  ( NASDAQ:META ) ",
+        summary : "Apple Inc. AAPL is reportedly ramping up its efforts to compete with Meta Platforms Inc. META. The iPhone maker's discounted version of the first-generation mixed-reality headset, Vision Pro, could be ready by next year.",
+        time_published : "20241014T020443",
+        topics : [
+            {relevance_score:  "1.0",
+            topic: "Technology"}
+        ],
+        url : "https://www.benzinga.com/news/24/10/41306527/apples-low-cost-vision-pro-could-hit-the-shelves-next-year-everything-you-need-to-know-about-iphone"
 
+    }
+]
 const News = () => {
     const [newsDataRetrieved, setNewsDataRetrieved] = useState([]);
-    useEffect(() => {
-        const retrieveNewsData = async () => {
-            const ticker = 'AAPL';
-            const retrievedData = await fetchNewsData();
-            setNewsDataRetrieved(retrievedData.feed);
-        }
-        retrieveNewsData();
-    }, []);
+
+    const [testNewsData, setTestNewsData] = useState(newsData);
+    const [selectedNews, setSelectedNews] = useState(newsData[0]);
+
+    const [textInput, setTextInput] = useState('');
+    const retrieveNewsData = async () => {
+        const retrievedData = await fetchNewsData(textInput);
+        console.log(retrievedData)
+        setNewsDataRetrieved(retrievedData.feed);
+    }
+
     return (
         <Container fluid>
-            { newsDataRetrieved ? <Row>
+            {/* { newsDataRetrieved ? <Row>
                 <Col sm={12} className="news-list-column">
                     {newsDataRetrieved.map((newsItem, index) => (
                         <InformationCard
@@ -33,7 +54,34 @@ const News = () => {
                 </Col>
 
             </Row> : "Unable to retrieve news data currently"
-            }
+            } */}
+            {/* Refactor search bar */}
+            <div className="search-bar">
+            <input
+                type="text"
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value.toUpperCase())}
+                placeholder="Enter stock symbol"
+            />
+            <button onClick={retrieveNewsData}>Add</button>
+            {/* {textInput && <AutoCompleteSuggestion query={textInput} onSelect={handleSelectSuggestion} />} */}
+            </div>
+            { testNewsData ? <Row>
+                            <Col sm={12} className="news-list-column">
+                                {testNewsData.map((newsItem, index) => (
+                                    <InformationCard
+                                        title={newsItem.title}
+                                        bodyText={newsItem.summary}
+                                        source={newsItem.source}
+                                        sourceDomain={newsItem.source_domain}
+                                        image={newsItem.banner_image}
+                                        newsUrl={newsItem.url}
+                                    />
+                                ))}
+                            </Col>
+
+                        </Row> : "Unable to retrieve news data currently"
+                        }
         </Container>
     )
 }
--- src/services/NewsService.jsx
+++ src/services/NewsService.jsx
@@ -4,12 +4,12 @@ const API_KEY_BACKUP = 'process.env.API_KEY_BACKUP';
 const BASE_URL = 'https://www.alphavantage.co/query';
 const searchAlphaVantageURL = `${BASE_URL}/query`;
 
-export const fetchNewsData = async () => {
+export const fetchNewsData = async (symbol) => {
     try {
         const response = await axios.get('https://www.alphavantage.co/query', {
             params: {
               function: 'NEWS_SENTIMENT',
-              tickers: 'AAPL',
+              tickers: symbol,
               apikey: API_KEY,
             },
           });
